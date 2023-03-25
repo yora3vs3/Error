520dff220512a14ee2a2eac5cf52032aac30504d
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2023-03-25 13:46:47 +0300

    errorTools

---
+++ server.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, render_template, jsonify
+import csv
+import os
+
+app = Flask(__name__, template_folder='public', static_folder='public')
+
+
+# Define the path to the CSV file
+CSV_FILE = 'tools.csv'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/script.js')
+def script():
+    return send_file('public/script.js', mimetype='text/javascript')
+
+
+# Define the route to handle POST requests to add a tool
+
+
+@app.route('/tools', methods=['POST'])
+def add_tool():
+    # Get the tool data from the request body
+    data = request.get_json()
+    name = data['name']
+    price = data['price']
+
+    # Write the tool data to the CSV file
+    with open(CSV_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([name, price])
+
+    # Return the new tool data as JSON
+    return jsonify({'name': name, 'price': price})
+
+# Define the route to handle GET requests to load the tools
+
+
+@app.route('/tools', methods=['GET'])
+def get_tools():
+    # Read the tools from the CSV file
+    tools = []
+    with open(CSV_FILE, 'r', newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            tools.append({'name': row[0], 'price': float(row[1])})
+
+    # Return the tools as JSON
+    return jsonify(tools)
+
+if 'VERCEL_ENV' in os.environ:
+    app.config['ENV'] = 'production'
+
+
+
+if __name__ == '__main__':
+    # Run the server
+    app.run(debug=True)
